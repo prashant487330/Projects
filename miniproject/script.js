e67ca875da432554e78af61a72b02ac0c49965e4
prashant patel <prashantpatel487330@gmail.com>
2025-07-05 19:42:09 +0530

    mini project

---
+++ miniproject/script.js
@@ -0,0 +1,18 @@
+let fun1=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(https://www.imagineonline.store/cdn/shop/files/iPhone_16_Pro_Max_Desert_Titanium_PDP_Image_Position_1b__en-IN_bfbeb796-1c49-4692-a68f-aaeaa75b8de9.jpg?v=1727250896&width=823)"
+}
+
+let fun2=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/tile/Apple-iPhone-16-Pro-hero-240909-lp.jpg.news_app_ed.jpg)"
+}
+
+let fun3=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-16-pro-max-2.jpg)"
+}
+let fun4=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(https://akm-img-a-in.tosshub.com/indiatoday/images/story/202410/apple-iphone-16-pro-max-104808808-1x1.jpg?VersionId=A4mJaTK5YHrbtY7bBuGRIf_x94zAC5pZ)"   
+}
